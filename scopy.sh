64270d8cc01a86e92300d729bdf11f9aa7db1373
Ubuntu <ubuntu@ip-172-31-1-112.ap-south-1.compute.internal>
2023-05-03 15:31:24 +0000

    adding scopy file

--- scopy.sh
+++ scopy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-scp -r /var/lib/jenkins/workspace/build-java-project/target/TrainBook-1.0.0-SNAPSHOT.war ubuntu@172.31.11.73:/home/ubuntu/war-files/
+scp -i /home/ubuntu/key/key.pem -r /var/lib/jenkins/workspace/build-java-project/target/TrainBook-1.0.0-SNAPSHOT.war ubuntu@172.31.11.73:/home/ubuntu/war-files/
