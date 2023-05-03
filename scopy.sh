a771026516168ab6bf8dc1d368c5f3ddf2883146
Ubuntu <ubuntu@ip-172-31-1-112.ap-south-1.compute.internal>
2023-05-03 15:56:48 +0000

    adding scopy 1.4

--- scopy.sh
+++ scopy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-scp -o StrictHostKeyChecking=no -i /var/lib/jenkins/keys/keys.pem -r /var/lib/jenkins/workspace/build-java-project/target/TrainBook-1.0.0-SNAPSHOT.war ubuntu@172.31.11.73:/home/ubuntu/war-files/
+scp -o StrictHostKeyChecking=no -i /var/lib/jenkins/keys/keys.pem -r /var/lib/jenkins/workspace/build-java-project/target/TrainBook-1.0.0-SNAPSHOT.war ubuntu@172.31.11.73:/opt/tomcat/webapps/
